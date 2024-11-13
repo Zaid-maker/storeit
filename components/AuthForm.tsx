a4d4426290d1134cc6106c6cd5a74c0e9591bc37
Zaid-maker <pzhafeez@gmail.com>
2024-11-13 22:03:10 +0500

    AuthForm component

---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+type FormType = "sign-up" | "sign-in";
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
