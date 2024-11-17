b43e14b1509a3df31624232c868aa61182b8de9e
Zaid-maker <pzhafeez@gmail.com>
2024-11-17 19:02:46 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 // Home
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      Home
+      <h1 className="h1 mb-3">Welcome to StoreIt</h1>
+      <div className="flex gap-4">
+        <Button variant="default">
+          <Link href="/sign-in">Sign In</Link>
+        </Button>
+        <Button variant="default">
+          <Link href="/sign-up">Sign Up</Link>
+        </Button>
+      </div>
     </main>
   );
 }
