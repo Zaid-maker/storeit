f96645ec816dc23e5adc2cb3c7ec0ffdaac737ea
Zaid-maker <pzhafeez@gmail.com>
2024-11-18 17:34:09 +0500

    auth appwrite

---
+++ components/lib/actions/user.actions.ts
@@ -0,0 +1,11 @@
+"use server";
+
+const getUserByEmail = async (email: string) => {
+    
+}
+
+export const createUser =
+  async () =>
+  ({ fullName, email }: { fullName: string; email: string }) => {
+    return;
+  };
