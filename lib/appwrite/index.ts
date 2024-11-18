3c96b06aec64ab21a67e5df2a2b70e12b8ee0b99
Zaid-maker <pzhafeez@gmail.com>
2024-11-18 18:08:20 +0500

    appwrite createAdminClient

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,2 +1,27 @@
-"use server"
+"use server";
 
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
+
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
