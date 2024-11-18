065096bc7d7faa89d526f4353b949083fbefbbbf
Zaid-maker <pzhafeez@gmail.com>
2024-11-18 23:57:48 +0500

    update layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,9 +4,9 @@ export const dynamic = "force-dynamic";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <main className="min-h-screen">
       {children}
-    </div>
+    </main>
   );
 };
 
