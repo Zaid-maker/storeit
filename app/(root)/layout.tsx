16c76f9324b1d349e10995e87a90829e10f6802e
Zaid-maker <pzhafeez@gmail.com>
2024-11-19 21:59:50 +0500

    if user is not logged in redirect to sign in

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,18 @@
 import { Sidebar } from "@/components/Sidebar";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export const dynamic = "force-dynamic";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
   return (
     <main className="min-h-screen">
-      <Sidebar />
+      <Sidebar {...currentUser} />
 
       <section className="flex h-full flex-1 flex-col">
         <div className="main-content">{children}</div>
