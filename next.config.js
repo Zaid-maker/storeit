2d13b2af5c0552a877d61b930019af61b034f506
Zaid-maker <pzhafeez@gmail.com>
2024-11-20 17:36:09 +0500

    Fix i guess

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Handle font loading
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: {
-        loader: "url-loader",
-        options: {
-          limit: 8192,
-          publicPath: "/_next/static/fonts/",
-          outputPath: "static/fonts/",
-          name: "[name].[hash:8].[ext]",
-        },
-      },
-    });
-    return config;
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
