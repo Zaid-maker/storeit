994beefbe117cb419738e239b878cf51586c37de
Zaid-maker <pzhafeez@gmail.com>
2024-12-08 01:20:00 +0500

    update Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { signOutUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 type HeaderProps = {
   userId: string;
@@ -6,7 +8,31 @@ type HeaderProps = {
 };
 
 const Header = ({ userId, accountId }: HeaderProps) => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      {/* <Search /> */}
+      <div className="header-wrapper">
+        {/* <FileUploader userId={userId} accountId={accountId} /> */}
+        <form
+          action={async () => {
+            "use server";
+
+            await signOutUser();
+          }}
+        >
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
