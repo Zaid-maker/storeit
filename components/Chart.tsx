b3c354faff8b19cef495c3b257c3fef536ec28d2
Zaid-maker <pzhafeez@gmail.com>
2024-12-08 00:24:34 +0500

    test

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+"use client";
 
-export const Chart = () => {
+import {
+  Label,
+  PolarGrid,
+  PolarRadiusAxis,
+  RadialBar,
+  RadialBarChart,
+} from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+import { calculatePercentage, convertFileSize } from "@/lib/utils";
+
+const chartConfig = {
+  size: {
+    label: "Size",
+  },
+  used: {
+    label: "Used",
+    color: "white",
+  },
+} satisfies ChartConfig;
+
+export const Chart = ({ used = 0 }: { used: number }) => {
+  const chartData = [{ storage: "Used", 10: used, fill: "white" }];
+
   return (
-    <div className='chart'>Chart</div>
-  )
-}
+    <Card className="chart">
+      <CardContent className="flex-1 p-0">
+        <ChartContainer config={chartConfig} className="chart-container">
+          <RadialBarChart
+            data={chartData}
+            startAngle={90}
+            endAngle={Number(calculatePercentage(used)) + 90}
+            innerRadius={80}
+            outerRadius={110}
+          >
+            <PolarGrid
+              gridType="circle"
+              radialLines={false}
+              stroke="none"
+              className="polar-grid"
+            />
+          </RadialBarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
