225604bb4163115cfbfda04481fa296247fb740a
Zaid-maker <pzhafeez@gmail.com>
2024-12-08 01:12:16 +0500

    Fix layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,12 +11,16 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
   if (!currentUser) return redirect("/sign-in");
 
   return (
-    <main className="min-h-screen">
+    <main className="flex h-screen">
       <Sidebar {...currentUser} />
 
       <section className="flex h-full flex-1 flex-col">
+        {/* <MobileNavigation {...currentUser} /> */}
+        {/* <Header userId={currentUser.$id} accountId={currentUser.accountId} /> */}
         <div className="main-content">{children}</div>
       </section>
+
+      {/** <Toaster /> */}
     </main>
   );
 };
